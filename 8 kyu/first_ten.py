8f1f97e9e78e530b6f7e6b71ed62dd64eb692b55
makrotheeast <makrotheeast@gmail.com>
2022-12-08 08:42:27 +0300

    New 5 8 kyu tasks were solved

--- 8 kyu/first_ten.py
+++ 8 kyu/first_ten.py
@@ -21,3 +21,29 @@ def opposite(num):
 def bool_to_word(boolean):
     """Returns 'Yes' for True and 'No' for False"""
     return 'Yes' if boolean == True else 'No'
+
+
+def square_sum(numbers):
+    """Returns sum of squares of numbers from iteratable numbers"""
+    return sum([x ** 2 for x in numbers])
+
+
+def summation(number):
+    """Returns a sum of all pozitive numbers from one to number
+    both included"""
+    return sum([x for x in range(1, number + 1)])
+
+
+def no_space(string : str):
+    """Returns a string without spaces"""
+    return ''.join(string.split(' '))
+
+
+def find_smallest_int(numbers):
+    """Returns the smallest int in numbers"""
+    return min(numbers)
+
+
+def count_sheeps(sheeps):
+    """Returns True number in sheeps"""
+    return sum(x for x in sheeps if x != None)
